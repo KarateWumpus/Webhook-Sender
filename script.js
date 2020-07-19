7872bdcd36be9316795329d23424fbf240c30d09
KarateWumpus <48181821+KarateWumpus@users.noreply.github.com>
2020-07-19 11:10:30 +0200

    Error Handling and new CSS

--- script.js
+++ script.js
@@ -11,29 +11,45 @@ form.addEventListener('submit', (event) => {
     const content = formData.get('content')
     const API_URL = formData.get('webhook_url')
     const resetForm = formData.get('resetForm')
-    if(resetForm){
-        form.reset()
-    }
     const body = {
         username,
         avatar_url,
         content
     }
-    fetch(API_URL + '?wait=1', {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(async (response) => {
-            const obj = await response.json()
-            if(response.status === 200) {
-                const str = JSON.stringify(obj, null, 4);
-                confirmation.innerHTML = `<h3>Success</h3>`
-                textArea.style.visibility = 'visible'
-                textArea.innerHTML = str
-                hljs.highlightBlock(textArea)
+    const allowedURLS = ['canary.discord.com', 'ptb.discord.com', 'discord.com']
+    const checkURL = new URL(API_URL)
+    if (!allowedURLS.includes(checkURL.host)) {
+        confirmation.innerHTML = `<h3>Error: Not a valid webhook URL</h3>`
+        return
+    }
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(result => {
+            if (result.type !== 1) {
+                confirmation.innerHTML = `<h3>Error: Not a valid webhook URL`
+            } else {
+                fetch(API_URL + '?wait=1', {
+                        method: "POST",
+                        body: JSON.stringify(body),
+                        headers: {
+                            'content-type': 'application/json'
+                        }
+                    })
+                    .then(async (response) => {
+                        const obj = await response.json()
+                        if (response.status === 200) {
+                            const str = JSON.stringify(obj, null, 4);
+                            confirmation.innerHTML = `<h3>Success</h3>`
+                            textArea.style.visibility = 'visible'
+                            textArea.innerHTML = str
+                            hljs.highlightBlock(textArea)
+                            if (resetForm) {
+                                form.reset()
+                            }
+                        } else {
+                            confirmation.innerHTML = `<h3>Error ${response.status}</h3>`
+                        }
+                    })
             }
         })
 })
