c36406ddc5f53cdd9614e08657c8e4eda693fa14
KarateWumpus <48181821+KarateWumpus@users.noreply.github.com>
2020-07-19 15:45:06 +0200

    Consistency and removal of GET request

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.hook-form')
 const textArea = document.querySelector('.json')
 const confirmation = document.querySelector('.confirmation')
+const allowedURLS = ['canary.discord.com', 'ptb.discord.com', 'discord.com', 'canary.discordapp.com', 'ptb.discordapp.com', 'discordapp.com']
 textArea.style.visibility = 'hidden'
 
 form.addEventListener('submit', (event) => {
@@ -17,44 +18,39 @@ form.addEventListener('submit', (event) => {
         avatar_url,
         content
     }
-    const allowedURLS = ['canary.discord.com', 'ptb.discord.com', 'discord.com', 'canary.discordapp.com', 'ptb.discordapp.com', 'discordapp.com']
     const checkURL = new URL(API_URL)
+    const checkPathname = checkURL.pathname.split('/')
     if (!allowedURLS.includes(checkURL.host)) {
-        confirmation.innerHTML = `<h3>Error: Not a valid webhook URL</h3>`
+        confirmation.innerHTML = '<h3>Error: Not a valid webhook URL</h3>'
+        return
+    } else if (checkPathname[1] + checkPathname[2] !== 'apiwebhooks') {
+        confirmation.innerHTML = '<h3>Error: Not a valid webhook URL</h3>'
         return
     }
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(result => {
-            if (result.type !== 1) {
-                confirmation.innerHTML = `<h3>Error: Not a valid webhook URL`
+    fetch(API_URL + '?wait=1', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(async (response) => {
+            const obj = await response.json()
+            if (response.status === 200) {
+                const str = JSON.stringify(obj, null, 4)
+                confirmation.innerHTML = '<h3>Success</h3>'
+                textArea.style.visibility = 'visible'
+                textArea.innerHTML = str
+                hljs.highlightBlock(textArea)
+                if (resetForm) {
+                    form.reset()
+                }
             } else {
-                fetch(API_URL + '?wait=1', {
-                        method: "POST",
-                        body: JSON.stringify(body),
-                        headers: {
-                            'content-type': 'application/json'
-                        }
-                    })
-                    .then(async (response) => {
-                        const obj = await response.json()
-                        if (response.status === 200) {
-                            const str = JSON.stringify(obj, null, 4);
-                            confirmation.innerHTML = `<h3>Success</h3>`
-                            textArea.style.visibility = 'visible'
-                            textArea.innerHTML = str
-                            hljs.highlightBlock(textArea)
-                            if (resetForm) {
-                                form.reset()
-                            }
-                        } else {
-                            confirmation.innerHTML = `<h3>Error ${response.status}</h3>`
-                            const str = JSON.stringify(obj, null, 4);
-                            textArea.style.visibility = 'visible'
-                            textArea.innerHTML = str
-                            hljs.highlightBlock(textArea)
-                        }
-                    })
+                confirmation.innerHTML = `<h3>Error ${response.status}</h3>`
+                const str = JSON.stringify(obj, null, 4)
+                textArea.style.visibility = 'visible'
+                textArea.innerHTML = str
+                hljs.highlightBlock(textArea)
             }
         })
 })
