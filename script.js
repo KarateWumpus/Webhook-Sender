ddebf8b280e5a6568ea6e43e4c3a1a6bf2dbffb2
KarateWumpus <48181821+KarateWumpus@users.noreply.github.com>
2020-07-28 11:26:11 +0200

    Embeds

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const form = document.querySelector('.hook-form')
 const textArea = document.querySelector('.json')
 const themeButton = document.querySelector('.themeButton')
 const themeSwitch = document.querySelector('.themeSwitch')
+const embed = document.querySelector('.embed')
+const embedDesc = document.querySelector('#embed-description')
+const addEmbedSwitch = document.querySelector('.addEmbed')
 const confirmation = document.querySelector('.confirmation')
 const allowedURLS = ['canary.discord.com', 'ptb.discord.com', 'discord.com', 'canary.discordapp.com', 'ptb.discordapp.com', 'discordapp.com']
 textArea.style.visibility = 'hidden'
@@ -15,10 +18,48 @@ form.addEventListener('submit', (event) => {
     const content = formData.get('content')
     const API_URL = formData.get('webhook_url')
     const resetForm = formData.get('resetForm')
+    let embeds
+    if(!embed.hidden) {
+        const title = formData.get('title')
+        const description = formData.get('description')
+        const color = parseInt(formData.get('color').substring(1), 16)
+        const author = formData.get('author')
+        const author_url = formData.get('author_url')
+        const author_icon = formData.get('author_icon')
+        const footer = formData.get('footer')
+        const footer_icon = formData.get('footer_icon')
+        const url = formData.get('url')
+        const image_url = formData.get('image_url')
+        const thumbnail_url = formData.get('thumbnail_url')
+        embeds = [
+            {
+                title,
+                description,
+                url,
+                color,
+                'author': {
+                    'name': author,
+                    'url': author_url,
+                    'icon_url': author_icon
+                },
+                'footer': {
+                    'text': footer,
+                    'icon_url': footer_icon
+                },
+                'image': {
+                    'url': image_url
+                },
+                'thumbnail': {
+                    'url': thumbnail_url
+                }
+            }
+        ]
+    }
     const body = {
         username,
         avatar_url,
-        content
+        content,
+        embeds
     }
     const checkURL = new URL(API_URL)
     const checkPathname = checkURL.pathname.split('/')
@@ -67,5 +108,15 @@ themeButton.addEventListener('click', (event) => {
     } else {
         themeSwitch.href = 'dark.css'
         themeButton.src = 'images/sun.png'
+    }
+})
+
+addEmbedSwitch.addEventListener('click', (event) => {
+    if(embed.hidden) {
+        embed.hidden = false
+        embedDesc.required = true
+    } else {
+        embed.hidden = true
+        embedDesc.required = false
     }
 })
